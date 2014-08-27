60674c6955dc5c4ed66006fe32c193829ccb727c
fey- <matt.carr@utexas.edu>
2014-08-27 10:49:42 -0500

    Update fv_authrestart.sh

--- fv_authrestart.sh
+++ fv_authrestart.sh
@@ -1,5 +1,12 @@
 #!/usr/local/bin/expect -f
 
+# I steal things from the internet and then fail at making them better.
+#
+# Matt Carr
+# matt.carr@utexas.edu
+#
+# In conjunction with a logout script that bypasses the filevault login screen.
+
 auth_pass="YOURPASSWORD"
 spawn sudo fdesetup -authrestart 
 expect  ":"
